b70ace54b812669ca38ff61bae9f7ce573a3c87e
lunarzDev <lunarzDev@outlook.com>
2024-10-18 17:33:46 -0500

    Removed tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -5,27 +5,44 @@ import './App.css'
 
 const App = () => {
   useEffect(() => {
+    
     // Set up scene, camera, and renderer
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     const renderer = new THREE.WebGLRenderer();
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
-    camera.position.setZ(25);
 
     document.body.appendChild(renderer.domElement);
+    // Add classes to Three canvas
+    renderer.domElement.classList.add('top-0', 'left-0', 'w-full', 'h-full', 'z-0');
+
+    // Assigns class values like in CSS
+    Object.assign(renderer.domElement.style, {
+      position: 'fixed',
+      top: '0',
+      left: '0',
+      width: '100%',
+      height: '100%',
+      zIndex: '-1'
+    });
 
     // Define an object (MeshStandardMaterial reacts to light, basic does not)
     const geometry = new THREE.TorusGeometry(10,3,16,100);
-    const material = new THREE.MeshStandardMaterial({ color: 0xFF6347 });
+    const material = new THREE.MeshStandardMaterial({ color: 0xA0A0FF });
     const torus = new THREE.Mesh(geometry, material);
     scene.add(torus);
     
+    //create a texture
+    const spaceTexture = new THREE.TextureLoader().load('space.jpg')
+    // set the texture for the scene's background to the spaceTexture
+    scene.background = spaceTexture;
+
     // a type of light that is concentrated from one spot
     const pointLight = new THREE.PointLight(0xffffff, 80);
     pointLight.position.set(5,15,5);
     // lights up the whole environment
-    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+    const ambientLight = new THREE.AmbientLight(0xffffff, 1);
     scene.add(pointLight, ambientLight);
 
     // shows us where the lights are
@@ -37,6 +54,17 @@ const App = () => {
     // adds ability to interact with the scene
     const controls = new OrbitControls(camera, renderer.domElement);
 
+    function moveCamera() {
+      const t = document.body.getBoundingClientRect().top;
+      
+      camera.position.z = t * 0.1 + 50;
+      camera.position.x = t * -0.2; // 
+      camera.rotation.y = t * -0.002;
+    }
+
+    moveCamera();
+
+    document.body.onscroll = moveCamera;
 
     function addStar() {
       const geometry = new THREE.SphereGeometry(0.25, 24, 24);
@@ -49,6 +77,7 @@ const App = () => {
       scene.add(star);
     }
 
+    // randomly generates 200 stars (spheres)
     Array(200).fill().forEach(addStar);
 
     // animation function that continually runs and renders out the scene
@@ -71,7 +100,47 @@ const App = () => {
     };
   }, []);  // Empty dependency array, so the effect runs once when the component mounts
 
-  return null;  // This component doesn't render any JSX, just the Three.js canvas
+  /*return (
+    <main>
+      <header>
+        <h1>Liam Bray</h1>
+        <p>Welcome to my website!</p>
+      </header>
+      
+      <blockquote>
+        <p>I like making stuff and putting it on the internet</p>
+      </blockquote>
+  
+      <section>
+        <h2>Work History</h2>
+        
+        <div>
+          <h3>McDonalds</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
+          </p>
+        </div>
+        <div>
+          <h3>Burger King</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
+          </p>
+        </div>
+        <div>
+          <h3>Taco Bell</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
+          </p>
+        </div>
+      </section>
+  
+      <blockquote>
+        <p>Thanks for watching!</p>
+      </blockquote>
+    </main>
+  );*/
+  
+  return (null);
 };
-
+  
 export default App;
