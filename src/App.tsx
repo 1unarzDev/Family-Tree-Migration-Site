b9adbc9d8e0d10e877f210f3cc8bbf23169680d5
lunarzDev <lunarzDev@outlook.com>
2024-10-26 15:48:14 -0500

    Tenth Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
+// IMPORTS //
 import * as THREE from 'three';
-import { useEffect, useMemo } from 'react';
+import { useEffect } from 'react';
 // @ts-ignore
 import { getFresnelMat } from "/src/getFresnelMat.js";
 // @ts-ignore
@@ -7,84 +8,83 @@ import getStarfield from "/src/getStarfield.js";
 import './App.css';
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-
 gsap.registerPlugin(ScrollTrigger);
 
-const App = () => {
+// Define Static Variables //
+const textureLoader = new THREE.TextureLoader();
 
-  const textureLoader = new THREE.TextureLoader();
+const earthMesh = new THREE.Mesh(
+  new THREE.IcosahedronGeometry(10, 12),
+  new THREE.MeshPhongMaterial({
+    map: textureLoader.load("src/assets/textures/earthmap4k.jpg"),
+    specularMap: textureLoader.load("src/assets/textures/earthspec4k.jpg"),
+    bumpMap: textureLoader.load("src/assets/textures/earthbump4k.jpg"),
+    bumpScale: 10,
+  })
+);
 
+const lightsMesh = new THREE.Mesh(
+  new THREE.IcosahedronGeometry(10, 12), 
+  new THREE.MeshBasicMaterial({
+    map: textureLoader.load("src/assets/textures/earthlights4k.jpg"),
+    blending: THREE.AdditiveBlending,
+  })
+);
+
+const cloudsMesh = new THREE.Mesh(
+  new THREE.IcosahedronGeometry(10, 12),
+  new THREE.MeshStandardMaterial({
+    map: textureLoader.load("src/assets/textures/earthhiresclouds4K.jpg"),
+    alphaMap: textureLoader.load('src/assets/textures/earthcloudmaptrans.jpg'),
+    blending: THREE.AdditiveBlending,
+    transparent: true,
+    opacity: 0.8,
+  })
+);
+
+const glowMesh = new THREE.Mesh(
+  new THREE.IcosahedronGeometry(10, 12),
+  getFresnelMat()
+);
+
+// React App Component That Renders // 
+const App = () => {  
+
+  // Scene Initialization (Items That Will Change Over Time) //
   const createScene = () => {
     const scene = new THREE.Scene();
     const earthGroup = new THREE.Group();
     earthGroup.rotation.z = -23.4 * Math.PI / 180;
     scene.add(earthGroup);
 
-    const earthMesh = new THREE.Mesh(
-      new THREE.IcosahedronGeometry(10, 12),
-      new THREE.MeshPhongMaterial({
-        map: textureLoader.load("src/assets/textures/earthmap10k.jpg"),
-        specularMap: textureLoader.load("src/assets/textures/earthspec10k.jpg"),
-        bumpMap: textureLoader.load("src/assets/textures/earthbump10k.jpg"),
-        bumpScale: 0.01,
-      })
-    );
-    earthGroup.add(earthMesh);
-
-    const lightsMesh = new THREE.Mesh(
-      new THREE.IcosahedronGeometry(10, 12), 
-      new THREE.MeshBasicMaterial({
-        map: textureLoader.load("src/assets/textures/earthlights10k.jpg"),
-        blending: THREE.AdditiveBlending,
-      })
-    );
-    earthGroup.add(lightsMesh);
-
-    const cloudsMesh = new THREE.Mesh(
-      new THREE.IcosahedronGeometry(10, 12),
-      new THREE.MeshStandardMaterial({
-        map: textureLoader.load("src/assets/textures/earthhiresclouds4K.jpg"),
-        alphaMap: textureLoader.load('src/assets/textures/earthcloudmaptrans.jpg'),
-        blending: THREE.AdditiveBlending,
-        transparent: true,
-        opacity: 0.8,
-      })
-    );
     cloudsMesh.scale.setScalar(1.01);
-    earthGroup.add(cloudsMesh);
-
-    const glowMesh = new THREE.Mesh(
-      new THREE.IcosahedronGeometry(10, 12),
-      getFresnelMat()
-    );
     glowMesh.scale.setScalar(1.012);
+
+    earthGroup.add(earthMesh);
+    earthGroup.add(lightsMesh);
+    earthGroup.add(cloudsMesh);
     earthGroup.add(glowMesh);
 
     const sunlight = new THREE.DirectionalLight(0xFFFFFF, 2);
-    sunlight.position.set(-2, 0.5, 1.5);
+    sunlight.position.set(-2, -0.5, 2.6);
     scene.add(sunlight);
 
-    const tl = gsap.timeline({
-      scrollTrigger: {
-        trigger: ".section-two",
-        endTrigger: ".section-five",
-        scrub: true,
-      },
-    });
-
-    return { scene, earthMesh, lightsMesh, cloudsMesh, tl };
+    return scene;
   };
 
+  //  Mounted Once And Only Reruns When The Dependency Array Changes //
   useEffect(() => {
-    const { scene, earthMesh, lightsMesh, cloudsMesh, tl } = createScene();
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    const scene = createScene();
+    scene.rotation.set(0, 0, 0);
+
     const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.toneMapping = THREE.ACESFilmicToneMapping;
     renderer.outputColorSpace = THREE.LinearSRGBColorSpace;
 
-    camera.position.set(2, 0, 50);
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.set(0, 0, 25);
     
     document.body.appendChild(renderer.domElement);
     renderer.domElement.classList.add('top-0', 'left-0', 'w-full', 'h-full', 'z-0');
@@ -96,7 +96,98 @@ const App = () => {
       height: '100%',
       zIndex: '999'
     });
+  
+    // GSAP Animation //
+    const updateTL = () => {
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: ".section-two",
+          endTrigger: ".section-five",
+          scrub: true,
+        },
+      });
+    
+      /* GSAP Crashcourse /*
 
+        gsap.from('.className', { duration: 1, y: '-100%', ease: 'bounce'})
+
+        animates every component that has the .className class
+        from the state that you give it to the state that it is
+        written in your code, this is mostly used for animations
+        when the website first loads in
+
+        the line above would animate from the bottom of the page
+        to the top with a duration of 1
+
+        ease has different preset curves from GSAP
+
+        the stagger property staggers each element in each class
+
+        the rest of these should be self-explanatory
+
+        gsap.from('.class2', { duration: 1, opacity: 0, delay: 1 })
+        gsap.from('.class3', { duration: 2, x: '-100vw', delay: 1, ease: 'power2.in' })
+
+        --------------------------------------------------------
+
+        gsap.to('.classTo', {duration: 1, y: 0, ease: 'elastic', delay: 2.5 })
+
+        animates to a certain position if you apply a transform to the class
+        .class2 {
+          transform: translateY(100%);
+        }
+
+        --------------------------------------------------------
+
+        gsap.fromTo('.class5', 
+                    { opacity: 0, scale: 0, rotation: 720 }, 
+                    { duration: 1, delay: 3.5, opacity: 1, scale: 1, rotation: 0 })
+
+        this is just a better version of gsap.to
+        because it allows you to specify the start
+        and end values in one line of code
+
+        --------------------------------------------------------
+
+        const obj = { x: 0 }
+        gsap.to(obj, { x: 100 , onUpdate: () => console.log(obj.x) })
+
+        onUpdate: () => runs every time you update, so it can be good for SVGs
+
+        --------------------------------------------------------
+
+        A timeline helps you line up all of these animations and set defaults
+
+        const timeline = gsaptimeline( {defaults: { duration: 1 }} )
+        timeline.from('.class1', { y: '-100%', ease: 'bounce' }).from('.nextClass' { opacity: 0, stagger: .5}, '<.5')
+
+        the animation now has the default value of the timeline
+
+        you can chain them together by just adding another .from after
+        just remember that you can override things
+
+        timelines also have absolute and relative delays from the previous item
+
+        if you just write a number after all of the parameters, it is absolute
+        if you use a carat, it references the animation in that direction
+        in this example, you go .5 seconds after the previous animation
+
+        you can easily reverse a timeline like so
+
+        const button = document.querySelector('.button')
+
+        button.addEventListener('click', () => {timeline.reverse()})
+
+      */
+
+      tl.to(scene.rotation, { y: 1.65 })
+        .to(scene.rotation, { y: 3.1 });
+
+      return tl;
+    };
+    const timeline = updateTL();
+
+    // Helper Functions //
     const onWindowResize = () => {
       camera.aspect = window.innerWidth / window.innerHeight;
       camera.updateProjectionMatrix();
@@ -112,29 +203,42 @@ const App = () => {
     };
     animate();
 
-    scene.rotation.set(0, 3.1, 0);
-  
-    tl
-      .to(scene.rotation, { y: 1.65 })
-      .to(scene.rotation, { y: 3.1 });  
-
+    // Cleans Up After The Animation Loop //
     return () => {
+      ScrollTrigger.getAll().forEach(trigger => trigger.kill());
+      timeline.kill();
       window.removeEventListener("resize", onWindowResize);
       renderer.dispose();
       scene.clear();
       document.body.removeChild(renderer.domElement);
     };
-  }, [textureLoader]);
+  }, []); // Add Dependencies (Items That Should Rerun UseEffect() Here)
 
+  // HTML Return Statement //
   return (
     <main>
-      <section className="section-one"></section>
+      <section className="section-one">
+        {/* <svg height="100px" width="200px" transform="translate(20,2.5)">
+          <filter id="grainy">
+            <feTurbulence type='fractalNoise' baseFrequency="0.6"/>
+            <feComposite operator="in" in2="SourceGraphic" result="monoNoise"/>
+          </filter>
+          <path 
+            xmlns="http://www.w3.org/2000/svg"  
+            d="M28.7,-46.9C40,-43.1,54,-41.2,63.6,-33.7C73.2,-26.2,78.4,-13.1,75.1,-1.9C71.7,9.2,59.9,18.5,53.1,30.9C46.3,43.2,44.6,58.7,36.6,70.1C28.6,81.5,14.3,88.8,-0.9,90.4C-16.1,91.9,-32.1,87.7,-45.6,79.4C-59.1,71.2,-69.9,58.9,-78,45C-86.1,31.1,-91.4,15.5,-86.5,2.8C-81.6,-9.9,-66.5,-19.8,-56.8,-30.9C-47.1,-42.1,-42.9,-54.6,-34.4,-60C-25.8,-65.4,-12.9,-63.8,-2.1,-60.1C8.7,-56.5,17.3,-50.8,28.7,-46.9Z"
+          />
+        </svg> */}
+      </section>
       <section className="section-two"></section>
       <section className="section-three"></section>
       <section className="section-four"></section>
       <section className="section-five"></section>
+      <section className="section-six"></section>
+      <section className="section-seven"></section>
+      <section className="section-eight"></section>
     </main>
   );
 };
 
+// App Export For Connections With Other Scripts //
 export default App;
